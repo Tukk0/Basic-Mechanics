a20913b0a2a21df4b36213ee9a24681a3c2603f0
Mihaham <misha19102005@yandex.ru>
2024-09-18 22:07:21 +0300

    Everyting works

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import pygame
 
 from math import sqrt
 
-from src.config import dt, G, RED, LENGTH, HIGHT, X, MIDX, MIDY
+from src.config import dt, G, RED, LENGTH, HIGHT, X, MIDX, MIDY, MZ, V1, V2, RZ
 
 class Vector:
     def __init__(self, x : int, y : int) -> None:
@@ -27,12 +27,13 @@ class Vector:
 
 
 class Object:
-    def __init__(self, mass : int, v : Vector, x : int, y : int, image : str = "sprites/empty.png") -> None:
+    def __init__(self, mass : int, v : Vector, x : int, y : int, image : str = "sprites/empty.png", size : int = 100) -> None:
         self.mass : int = mass
         self.v : Vector = v
         self.x : int = x
         self.y : int = y
         self.image : int = pygame.image.load(image).convert_alpha()
+        self.image = pygame.transform.scale(self.image,(size,size) )
     
     def get_mass(self) -> int:
         return self.mass
@@ -64,9 +65,9 @@ class Static(Object):
     def move(self):
         pass
 
-def F(object1, object2) -> Vector:
+def Func(object1, object2) -> Vector:
     view = Vector(object2.get_x() - object1.get_x(), object2.get_y() - object1.get_y())
-    k = G*(object1.get_mass()) * (object2.get_mass) / ((view.len())**2)
+    k = G*(object1.get_mass()) * (object2.get_mass()) / ((view.len())**2)
     view.normilize()
     return view.scalar(k)
     
@@ -74,8 +75,9 @@ def F(object1, object2) -> Vector:
 
 
 class Scene:
-    def _init_(self):
-        self.obj : list[Object]= [Object(1,Vector(1,1), 0,0), Static(1,Vector(1,1), 0,100)]
+    def __init__(self):
+        self.obj : list[Object] = [Object(100,Vector(V1, 0), 0,X*110 , "sprites/sputnic.png", size = 100),
+                                   Static(MZ,Vector(1,1), 0,0, "sprites/earth.png", size=100), ]
         self.len = len(self.obj)
 
     def update(self, surface):
@@ -85,7 +87,7 @@ class Scene:
             for j in range(i+1,self.len):
                 obj1 = self.obj[i]
                 obj2 = self.obj[j]
-                F = F(obj1, obj2)
+                F = Func(obj1, obj2)
                 a[i].increase(F.scalar(1/obj1.get_mass()))
                 a[j].increase(F.scalar(-1/obj2.get_mass()))
         
@@ -96,28 +98,23 @@ class Scene:
         self.Draw(surface)
 
     def Draw(self, surface):
-
-        def draw_text(self, surface, text, position):
-            text_skin = self.my_font.render(text, False, RED)
-            text_rect = text_skin.get_rect(
-                center=position,
-                width=self.scale)
+        surface.fill((0, 0, 0))
+        def draw_text(surface, text, position):
+            text_skin = pygame.font.SysFont('Comic Sans MS', 24).render(text, False, RED)
+            text_rect = text_skin.get_rect(center=position)
             surface.blit(text_skin, text_rect)
 
-        def draw_object(self, surface: pygame, object, pos_x, pos_y, draw_scale=1):
-            object_skin = self.get_image(object.get_skin())
-            object_skin = pygame.transform.scale(object_skin,
-                                                (draw_scale * self.scale, draw_scale * self.scale))
-            object_rect = object_skin.get_rect(
-                center=(pos_x + MIDX, pos_y + MIDY),
-                width=self.scale)
+        def draw_object(surface: pygame, object, pos_x, pos_y, draw_scale=1):
+            object_skin = object.get_skin()
+            object_rect = object_skin.get_rect(center=(pos_x + MIDX, pos_y + MIDY),)
             surface.blit(object_skin, object_rect)
 
         image = pygame.image.load("sprites/empty.png").convert_alpha()
 
         for obj in self.obj:
-            self.draw_object(surface, obj, obj.get_x() / X, obj.get_y() / X)
+            draw_object(surface, obj, obj.get_x() / X, obj.get_y() / X)
 
+        pygame.display.flip()
         pygame.display.update()
 
 
@@ -137,7 +134,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:  
                 keepGameRunning = False
-        scene.update()
+        scene.update(surface)
         
 
 if __name__ == "__main__":
