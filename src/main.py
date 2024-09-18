8d7c98582ab874071c574bb79ae223370a069d95
Михаил Городецкий <misha19102005@yandex.ru>
2024-09-18 20:43:30 +0300

    class Vector

--- src/main.py
+++ src/main.py
@@ -1,1 +1,20 @@
+import pygame
 
+class Vector:
+    def __init__(self, x : int, y : int) -> None:
+        self.x = x
+        self.y = y
+
+
+
+
+def main():
+    pygame.init()
+    keepGameRunning = True
+    while keepGameRunning:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:  
+                keepGameRunning = False
+
+if __name__ == "__main__":
+    main()
