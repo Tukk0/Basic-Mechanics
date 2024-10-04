c3a2736fee690a9138d946cf3ad586a3ae502ac0
mvideo <tikhoon@yandex.ru>
2024-10-04 15:21:08 +0300

    Energy calculation refactoring and documentation improvements

--- src/main.py
+++ src/main.py
@@ -20,9 +20,9 @@ class Vector:
         self.x += a.x
         self.y += a.y
 
-    # Scalar multiplication of vectors
-    def scalar(self, dt):
-        return Vector(self.x * dt, self.y * dt)
+    # Multiply a vector by a scalar
+    def resize(self, n):
+        return Vector(self.x * n, self.y * n)
 
     def square(self) -> float:
         return (self.x ** 2 + self.y ** 2)
@@ -30,7 +30,7 @@ class Vector:
     def length(self) -> float:
         return sqrt(self.square())
 
-    def normilize(self) -> None:
+    def normalize(self) -> None:
         k = self.length()
         if k != 0:
             self.x /= k
@@ -73,7 +73,8 @@ class Object:
         self.y += self.velocity.y * dt
 
     def change_velocity(self, a: Vector) -> None:
-        self.velocity.add(a.scalar(dt))
+        # The formula for updated velocity is v = v0 + a*dt
+        self.velocity.add(a.resize(dt))
         # Update kinetic energy
         self.k_energy = self.mass * self.velocity.square() / 2
 
@@ -106,11 +107,15 @@ class Static(Object):
         pass
 
 
-def Func(object1: Object, object2: Object, ind1: int, ind2: int) -> Vector:
-    view = Vector(object2.get_x() - object1.get_x(), object2.get_y() - object1.get_y())
-    k = vals.G * (object1.get_mass()) * (object2.get_mass()) / ((view.length()) ** 2)
-    view.normilize()
-    return view.scalar(k)
+def Gravitational_force(object1: Object, object2: Object) -> Vector:
+    # Get the directional vector of the gravitational force between objects
+    direction = Vector(object2.get_x() - object1.get_x(), object2.get_y() - object1.get_y())
+    # Find the magnitude of the force
+    gravitational_force = vals.G * (object1.get_mass()) * (object2.get_mass()) / ((direction.length()) ** 2)
+    # Normalize the vector
+    direction.normalize()
+    # Return the vector with the proper magnitude
+    return direction.resize(gravitational_force)
 
 
 class Scene:
@@ -122,81 +127,77 @@ class Scene:
             Object(SOLNCE, Vector(0, 0), 0, 0, "earth.png", size=100, name="2"),
             Object(7.36 * 10 ** 22, Vector(-1e6, 0), 0, D_ZM - 384.4 * 10 ** 6, "earth.png", size=100, name="3")]
         """
-
-        
-        # First speed
+        # First cosmic speed
         '''self.obj: list[Object] = [
             Object(83.6, Vector(V1, 0), 0, RZ + 160 * 10 ** 3, "sputnic.png", size=100, name="Sputnic"),
             Static(v.EARTH_M, Vector(0, 0), 0, 0, "earth.png", size=100, name="Earth"), ]'''
-        
-        # Second speed
+        # Second cosmic speed
         '''self.obj: list[Object] = [
             Object(83.6, Vector(V2, 0), 0, RZ + 160 * 10 ** 3, "sputnic.png", size=100, name="Sputnic"),
             Static(v.EARTH_M, Vector(0, 0), 0, 0, "earth.png", size=100, name="Earth"), ]'''
-        
-        #Smth between first and second speed
+        # Smth between first and second speed
         '''self.obj: list[Object] = [
             Object(83.6, Vector(V2 * 0.9, 0), 0, RZ + 160 * 10 ** 3, "sputnic.png", size=100, name = "Sputnic"),
             Static(v.EARTH_M, Vector(0, 0), 0, 0, "earth.png", size=100, name="Earth"), ]'''
-
-        # SUN and other planets
+        # Space objects
         self.obj: list[Object] = [
-            Object(vals.SUN_M, Vector(0, 0), 0, 0, "earth.png", img_size=100, name = "SUN"),
-            #Object(MERKURY, Vector(0, V_MERKURY), D_MERKURY, 0, "sputnic.png", size=100, name = "Merkury"),
-            #Object(VENERA, Vector(0, V_VENERA), D_VENERA, 0, "sputnic.png", size=100, name = "VENERA"),
-            Object(vals.EARTH_M, Vector(0, vals.EARTH_V), vals.EARTH_D, 0, "earth.png", img_size=100, name = "ZEMLYA"),
-            #Object(7.36 * 10 ** 22, Vector(0,V_ZM), D_ZM + 384.4 * 10 ** 6, 0, "sputnic.png", size=100, name="3"),
-            #Object(MARS, Vector(0, V_MARS), D_MARS, 0, "sputnic.png", size=100, name = "MARS"),
-            #Object(UPITER, Vector(0, V_UPITER), D_UPITER, 0, "sputnic.png", size=100, name = "UPITER"),
-            #Object(SATURN, Vector(0, V_SATURN), D_SATURN, 0, "sputnic.png", size=100, name = "SATURN"),
-            #Object(Uran, Vector(0, V_URAN), D_URAN, 0, "sputnic.png", size=100, name = "URAN"),
-            Object(vals.NEPTUNE_M, Vector(0, vals.NEPTUNE_V), vals.NEPTUNE_D, 0, "sputnic.png", img_size=100, name = "NEPTUN"),
+            Object(vals.SUN_M, Vector(0, 0), 0, 0, "sun.png", img_size=100, name = vals.SUN_NAME),
+            #Object(vals.MERCURY_M, Vector(0, vals.MERCURY_V), vals.MERCURY_D, 0, "sputnic.png", img_size=100, name = vals.MERCURY_NAME),
+            #Object(vals.VENUS_M, Vector(0, vals.VENUS_V), vals.VENUS_D, 0, "sputnic.png", img_size=100, name = vals.VENUS_NAME),
+            Object(vals.EARTH_M, Vector(0, vals.EARTH_V), vals.EARTH_D, 0, "earth.png", img_size=100, name = vals.EARTH_NAME),
+            Object(vals.MOON_M, Vector(0, vals.MOON_V), vals.MOON_D, 0, "sputnic.png", img_size=100, name = vals.MOON_NAME),
+            #Object(vals.MARS_M, Vector(0, vals.MARS_V), vals.MARS_D, 0, "sputnic.png", img_size=100, name = vals.MARS_NAME),
+            #Object(vals.JUPITER_M, Vector(0, vals.JUPITER_V), vals.JUPITER_D, 0, "sputnic.png", img_size=100, name = vals.JUPITER_NAME),
+            #Object(vals.SATURN_M, Vector(0, vals.SATURN_V), vals.SATURN_D, 0, "sputnic.png", img_size=100, name = vals.SATURN_NAME),
+            #Object(vals.URANUS_M, Vector(0, vals.URANUS_V), vals.URANUS_D, 0, "sputnic.png", img_size=100, name = vals.URANUS_NAME),
+            #Object(vals.NEPTUNE_M, Vector(0, vals.NEPTUNE_V), vals.NEPTUNE_D, 0, "sputnic.png", img_size=100, name = vals.NEPTUNE_NAME),
             ]
-        self.all_energy = sum(x.get_energy() for x in self.obj)
+        # The total energy of the system
+        self.total_energy = sum(x.get_energy() for x in self.obj)
         self.max_energy = None
         self.min_energy = None
-        self.len = len(self.obj)
-
+        # The number of objects in the system
+        self.system_size = len(self.obj)
+        # Current time
         self.time = time.time()
 
     def update(self, surface):
-        a = [Vector(0, 0) for i in self.obj]
-
-        for i in range(self.len):
-            for j in range(i + 1, self.len):
+        # Create an acceleration vector for every object in the system
+        accelerations_list = [Vector(0, 0) for i in self.obj]
+        # Calculate all the accelerations of the objects in the system
+        for i in range(self.system_size):
+            for j in range(i + 1, self.system_size):
                 obj1 = self.obj[i]
                 obj2 = self.obj[j]
-                F = Func(obj1, obj2, i, j)
-                a[i].add(F.scalar(1/obj1.get_mass()))
-                a[j].add(F.scalar(-1/obj2.get_mass()))
-
-        for i in range(self.len):
-            self.obj[i].change_velocity(a[i])
+                F = Gravitational_force(obj1, obj2)
+                # The formula for acceleration is a = F / m
+                # We add vectors, since acceleration is a sum of vectors
+                accelerations_list[i].add(F.resize(1/obj1.get_mass()))
+                # Two interacting objects have opposingly directed forces
+                accelerations_list[j].add(F.resize(-1/obj2.get_mass()))
+        # Update the positions of all the objects in the system
+        for i in range(self.system_size):
+            # Update the velocity
+            self.obj[i].change_velocity(accelerations_list[i])
+            # Update the position
             self.obj[i].move()
 
-        # update energy
-        view12 = Vector(self.obj[0].get_x() - self.obj[1].get_x(), self.obj[0].get_y() - self.obj[1].get_y())
-        view13 = Vector(self.obj[0].get_x() - self.obj[2].get_x(), self.obj[0].get_y() - self.obj[2].get_y())
-        view23 = Vector(self.obj[1].get_x() - self.obj[2].get_x(), self.obj[1].get_y() - self.obj[2].get_y())
-        self.obj[0].p_energy = - vals.G * (self.obj[0].get_mass()) * (self.obj[1].get_mass()) / (view12.length())
-        self.obj[1].p_energy = - vals.G * (self.obj[1].get_mass()) * (self.obj[2].get_mass()) / (view23.length())
-        self.obj[2].p_energy = - vals.G * (self.obj[0].get_mass()) * (self.obj[2].get_mass()) / (view13.length())
-        #v2 = Vector(self.obj[1].v.x, self.obj[1].y)
-        #v2.add(self.obj[0].v.scalar(-1))
-        #v3 = Vector(self.obj[2].v.x, self.obj[2].y)
-        #v3.add(self.obj[0].v.scalar(-1))
-        self.obj[0].k_energy = self.obj[0].get_mass() * (self.obj[0].get_velocity().square()) / 2
-        self.obj[1].k_energy = self.obj[1].get_mass() * (self.obj[1].get_velocity().square())/2
-        self.obj[2].k_energy = self.obj[2].get_mass() * (self.obj[2].get_velocity().square())/2
-        self.all_energy = sum(x.get_energy() for x in self.obj)
-        #if self.min_energy != None:
-        #    self.min_energy = min(self.all_energy, self.min_energy)
-        #    self.max_energy = max(self.all_energy, self.max_energy)
-        #else:
-        #    self.min_energy = self.all_energy
-        #    self.max_energy = self.all_energy
-        print(self.all_energy) #, self.min_energy, self.max_energy, self.max_energy - self.min_energy)
-
+        # Update objects` potential energies
+        for obj in self.obj:
+            obj.p_energy = 0
+        for i in range(self.system_size):
+            for j in range(i + 1, self.system_size):
+                obj1 = self.obj[i]
+                obj2 = self.obj[j]
+                direction = Vector(obj1.get_x() - obj2.get_x(), obj1.get_y() - obj2.get_y())
+                potential_energy = -vals.G * (obj1.get_mass()) * (obj2.get_mass()) / direction.length()
+                obj1.p_energy += potential_energy
+                obj2.p_energy += potential_energy
+        # Update objects` kinetic energies
+        for obj in self.obj:
+            obj.k_energy = obj.get_mass() * obj.get_velocity().square() / 2
+        # Update total energy
+        self.total_energy = sum(x.get_energy() for x in self.obj)
 
         if time.time() - self.time > 0.1:
             self.time = time.time() 
@@ -223,20 +224,20 @@ class Scene:
 
             start = (obj.get_x() / X + MIDX, obj.get_y() / X + MIDY)
             v = Vector(obj.get_velocity().x, obj.get_velocity().y)
-            v.normilize()
-            v = v.scalar(100)
+            v.normalize()
+            v = v.resize(100)
             v.add(Vector(start[0], start[1]))
 
             if VERBOSE > 0:
                 draw_object(surface, obj, start[0], start[1])
                 draw_text(surface, f"Speed of {obj.get_name()} : {round(obj.velocity.length(), 0)} м/s",
                       (LENGTH - 370, (i + 1) * 50))
-                #draw_text(surface, f"Energy : {self.all_energy} J", (50, (LENGTH - 370, 4 * 50)))
+                #draw_text(surface, f"Energy : {self.total_energy} J", (50, (LENGTH - 370, 4 * 50)))
                 draw_text(surface, f"{obj.get_name()}", (start[0], start[1] - obj.get_size() + 20), align = "center")
                 draw_text(surface, f"Position of the {obj.get_name()} is x : {round(obj.get_x(), 0)} m, y :"
                                    f"{round(obj.get_y(), 0)} m", (50, (i + 1) * 50))
                 if i == 0:
-                    draw_text(surface, f"Energy of system is : {self.all_energy} J", (50, (i + 4) * 50))
+                    draw_text(surface, f"Energy of system is : {self.total_energy} J", (50, (i + 4) * 50))
 
 
             if VERBOSE > 0:
